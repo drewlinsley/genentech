0f8815ce95c180c7def0d215dd1f039a801e7924
Drew Linsley <drewlinsley@gmail.com>
2022-02-22 10:45:38 -0500

    Fixed bug

--- src/pl_modules/model.py
+++ src/pl_modules/model.py
@@ -201,7 +201,7 @@ class MyModel(pl.LightningModule):
             - Tuple of dictionaries as described, with an optional 'frequency' key.
             - None - Fit will run without any optimizer.
         """
-        if if hasattr(self.cfg.optim.optimizer, "exclude_bn_bias") and \
+        if hasattr(self.cfg.optim.optimizer, "exclude_bn_bias") and \
                 self.cfg.optim.optimizer.exclude_bn_bias:
             params = self.exclude_from_wt_decay(self.named_parameters(), weight_decay=self.cfg.optim.optimizer.weight_decay)
         else:
