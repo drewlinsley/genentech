6c98446c1c600f11a5edf64e02d48ca5791c2cab
Drew Linsley <drewlinsley@gmail.com>
2022-04-10 12:50:04 -0400

    Added simclr resnets.

--- src/pl_modules/resnets.py
+++ src/pl_modules/resnets.py
@@ -10,8 +10,8 @@ from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
 
 # from pl_bolts.models.self_supervised.resnets import resnet18 as sclr_resnet18
 # from pl_bolts.models.self_supervised.resnets import resnet50 as scrl_resnet50
-from simclr_resnet_parts import resnet18 as sclr_resnet18
-from simclr_resnet_parts import resnet50 as scrl_resnet50
+from src.pl_modules.simclr_resnet_parts import resnet18 as sclr_resnet18
+from src.pl_modules.simclr_resnet_parts import resnet50 as scrl_resnet50
 # from pl_bolts.models.self_supervised import SimCLR
 
 
