62afb28b4a169a59289cc7cf118957cec5236024
Drew Linsley <drewlinsley@gmail.com>
2022-11-21 14:39:43 -0500

    Added new confs.

--- src/pl_modules/simclr_resnet_parts.py
+++ src/pl_modules/simclr_resnet_parts.py
@@ -272,6 +272,7 @@ class ResNet(nn.Module):
 def _resnet(arch, block, layers, pretrained, progress, **kwargs):
     model = ResNet(block, layers, **kwargs)
     if pretrained:
+        import pdb;pdb.set_trace()
         state_dict = load_state_dict_from_url(MODEL_URLS[arch], progress=progress)
         model.load_state_dict(state_dict, strict=False)
     return model
